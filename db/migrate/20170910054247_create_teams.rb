a8fcfc123040fde2e8b800f4674a21dfd1dd4c56
Nolan Lancaster <nolancaster@gmail.com>
2017-09-10 01:43:32 -0400

    generate team scaffold

---
+++ db/migrate/20170910054247_create_teams.rb
@@ -0,0 +1,11 @@
+class CreateTeams < ActiveRecord::Migration[5.1]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.references :league, foreign_key: true
+      t.references :owner, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170910054034) do
+ActiveRecord::Schema.define(version: 20170910054247) do
 
   create_table "leagues", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 20170910054034) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["league_id"], name: "index_owners_on_league_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.integer "league_id"
+    t.integer "owner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["league_id"], name: "index_teams_on_league_id"
+    t.index ["owner_id"], name: "index_teams_on_owner_id"
   end
 
 end
